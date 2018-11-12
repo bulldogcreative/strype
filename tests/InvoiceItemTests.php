9bcd26795c276c8ddf33ce042fdbe45826697fef
Levi Durfee <levi.durfee@gmail.com>
2018-11-12 13:03:33 -0500

    Add Invoice Items tests

--- tests/InvoiceItemTests.php
+++ tests/InvoiceItemTests.php
@@ -19,7 +19,7 @@ class InvoiceItemTests extends TestCase
         $this->id = new ObjectId();
     }
 
-    public function testCreateInvoiceItem()
+    public function testCreateInvoiceItemWithAmount()
     {
         $invoiceItem = $this->strype->invoiceItem()->create($this->customer,
             new \Bulldog\Strype\Resources\InvoiceItems\Amount(2500)
@@ -29,6 +29,65 @@ class InvoiceItemTests extends TestCase
         $this->assertEquals($this->customer->getCustomerId(), $invoiceItem->customer);
         $this->assertEquals('usd', $invoiceItem->currency);
         $this->assertInternalType('int', $invoiceItem->amount);
+    }
+
+    public function testCreateInvoiceItemWithQuantity()
+    {
+        $invoiceItem = $this->strype->invoiceItem()->create($this->customer,
+            new \Bulldog\Strype\Resources\InvoiceItems\Quantity(5, 500)
+        );
+        $this->assertEquals('invoiceitem', $invoiceItem->object);
+        $this->assertEquals(2500, $invoiceItem->amount);
+        $this->assertEquals($this->customer->getCustomerId(), $invoiceItem->customer);
+        $this->assertEquals('usd', $invoiceItem->currency);
+        $this->assertInternalType('int', $invoiceItem->amount);
+    }
+
+    public function testRetrieveInvoiceItem()
+    {
+        $invoiceItem = $this->strype->invoiceItem()->create($this->customer,
+            new \Bulldog\Strype\Resources\InvoiceItems\Amount(2500)
+        );
+        $retrieved = $this->strype->invoiceItem()->retrieve($invoiceItem->id);
+        $this->assertEquals('invoiceitem', $retrieved->object);
+        $this->assertEquals(2500, $retrieved->amount);
+        $this->assertEquals($this->customer->getCustomerId(), $retrieved->customer);
+        $this->assertEquals('usd', $retrieved->currency);
+        $this->assertInternalType('int', $retrieved->amount);
+    }
+
+    public function testUpdateInvoiceItem()
+    {
+        $invoiceItem = $this->strype->invoiceItem()->create($this->customer,
+            new \Bulldog\Strype\Resources\InvoiceItems\Amount(2500)
+        );
+        $updated = $this->strype->invoiceItem()->update($invoiceItem->id, [
+            'amount' => 3500
+        ]);
+        $this->assertEquals('invoiceitem', $updated->object);
+        $this->assertEquals(3500, $updated->amount);
+        $this->assertEquals($this->customer->getCustomerId(), $updated->customer);
+        $this->assertEquals('usd', $updated->currency);
+        $this->assertInternalType('int', $updated->amount);
+    }
+
+    public function testDeleteInvoiceItem()
+    {
+        $invoiceItem = $this->strype->invoiceItem()->create($this->customer,
+            new \Bulldog\Strype\Resources\InvoiceItems\Amount(2500)
+        );
+        $deleted = $this->strype->invoiceItem()->delete($invoiceItem->id);
+        $this->assertEquals('invoiceitem', $deleted->object);
+        $this->assertTrue($deleted->deleted);
+    }
+
+    public function testListAllInvoiceItems()
+    {
+        $items = $this->strype->invoiceItem()->listAll(['limit' => 3]);
+        foreach($items->data as $item) {
+            $this->assertEquals('invoiceitem', $item->object);
+        }
+        $this->assertCount(3, $items->data);
     }
 
     public function tearDown()
