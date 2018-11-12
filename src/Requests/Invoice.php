1898db8dca47409018912222d7772750267508d4
Levi Durfee <levi.durfee@gmail.com>
2018-11-12 15:13:58 -0500

    Add delete trait/interface

--- src/Requests/Invoice.php
+++ src/Requests/Invoice.php
@@ -6,16 +6,18 @@ use Bulldog\Strype\Request;
 use Bulldog\Strype\Traits\Retrieve;
 use Bulldog\Strype\Traits\Update;
 use Bulldog\Strype\Traits\ListAll;
+use Bulldog\Strype\Traits\Delete;
 use Bulldog\Strype\Contracts\Requests\CustomerInterface;
 use Bulldog\Strype\Contracts\Requests\InvoiceInterface;
 use Bulldog\Strype\Contracts\Traits\RetrieveInterface;
 use Bulldog\Strype\Contracts\Traits\UpdateInterface;
 use Bulldog\Strype\Contracts\Traits\ListAllInterface;
+use Bulldog\Strype\Contracts\Traits\DeleteInterface;
 use Bulldog\Strype\Contracts\Resources\SubscriptionBillingInterface;
 
-class Invoice extends Request implements InvoiceInterface, RetrieveInterface, UpdateInterface, ListAllInterface
+class Invoice extends Request implements InvoiceInterface, RetrieveInterface, UpdateInterface, ListAllInterface, DeleteInterface
 {
-    use Retrieve, Update, ListAll;
+    use Retrieve, Update, ListAll, Delete;
 
     public function create(CustomerInterface $customer, SubscriptionBillingInterface $type, array $arguments = [], string $key = null)
     {
