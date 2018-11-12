b4e0dea181778da4ced8705f374b8b0dc0b503d2
Levi Durfee <levi.durfee@gmail.com>
2018-11-12 14:34:48 -0500

    Add Invoice Interface

---
+++ src/Contracts/Requests/InvoiceInterface.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Bulldog\Strype\Contracts\Requests;
+
+use Bulldog\Strype\Contracts\Resources\InvoiceItemTypeInterface;
+
+interface InvoiceInterface
+{
+    public function create(CustomerInterface $customer, InvoiceItemTypeInterface $type, array $arguments = [], ?string $key);
+
+    public function finalize(string $invoiceid);
+
+    public function pay(string $invoiceid);
+
+    public function sendInvoice(string $invoiceid);
+
+    public function voidInvoice(string $invoiceid);
+
+    public function markUncollectable(string $invoiceid);
+
+    public function retrieveLineItems(string $invoiceid, array $arguments = []);
+
+    public function upcoming(CustomerInterface $customer, array $arguments = []);
+
+    public function retrieveUpcomingLineItems(CustomerInterface $customer, array $arguments = []);
+}
