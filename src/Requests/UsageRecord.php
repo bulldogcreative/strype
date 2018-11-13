79dec82bb0f5fe544e8c822a99dcc5af822d9d03
Levi Durfee <levi.durfee@gmail.com>
2018-11-13 14:26:48 -0500

    Create UsageRecord class

---
+++ src/Requests/UsageRecord.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Bulldog\Strype\Requests;
+
+use Bulldog\Strype\Request;
+use Bulldog\Strype\Contracts\Requests\UsageRecordInterface;
+
+class UsageRecord extends Request implements UsageRecordInterface
+{
+
+}
