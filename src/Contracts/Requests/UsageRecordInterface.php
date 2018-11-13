f5e0ca293a355b1240d0af3421b258a382b89b01
Levi Durfee <levi.durfee@gmail.com>
2018-11-13 14:29:44 -0500

    Add create method interface

--- src/Contracts/Requests/UsageRecordInterface.php
+++ src/Contracts/Requests/UsageRecordInterface.php
@@ -4,5 +4,5 @@ namespace Bulldog\Strype\Contracts\Requests;
 
 interface UsageRecordInterface extends \Bulldog\Strype\Contracts\RequestInterface
 {
-
+    public function create(int $quantity, SubscriptionItemInterface $subscriptionItem, int $timestamp, array $arguments = [], string $key = null);
 }
