65ea9ff01f116c5677e5d00d21f9490a54250538
Levi Durfee <levi.durfee@gmail.com>
2018-11-13 10:43:15 -0500

    Add Plan test file and test suite

---
+++ tests/PlanTests.php
@@ -0,0 +1,19 @@
+<?php
+
+include 'boot.php';
+
+use PHPUnit\Framework\TestCase;
+use Bulldog\Strype\Strype;
+use Bulldog\id\ObjectId;
+
+class PlanTests extends TestCase
+{
+    public $strype;
+    public $id;
+
+    public function setUp()
+    {
+        $this->strype = new Strype(getenv('STRIPE_API_KEY'));
+        $this->id = new ObjectId();
+    }
+}
