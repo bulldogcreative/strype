6a6a700eb4774ef9e51cb046c03354a8f3991a5b
Levi Durfee <levi.durfee@gmail.com>
2018-11-13 14:31:29 -0500

    Write UsageRecord create method

--- src/Requests/UsageRecord.php
+++ src/Requests/UsageRecord.php
@@ -3,9 +3,18 @@
 namespace Bulldog\Strype\Requests;
 
 use Bulldog\Strype\Request;
+use Bulldog\Strype\Contracts\Requests\SubscriptionItemInterface;
 use Bulldog\Strype\Contracts\Requests\UsageRecordInterface;
 
 class UsageRecord extends Request implements UsageRecordInterface
 {
+    public function create(int $quantity, SubscriptionItemInterface $subscriptionItem, int $timestamp, array $arguments = [], string $key = null)
+    {
+        $arguments['quantity'] = $quantity;
+        $arguments['subscription_item'] = $subscriptionItem->getId();
+        $arguments['timestamp'] = $timestamp;
+        $this->stripe('create', $arguments, $key);
 
+        return $this;
+    }
 }
