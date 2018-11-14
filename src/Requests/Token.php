bd342b223255c65603de8df9856073a2027d9b50
Levi Durfee <levi.durfee@gmail.com>
2018-11-14 15:51:50 -0500

    Update createBankAccount to match interface

--- src/Requests/Token.php
+++ src/Requests/Token.php
@@ -25,7 +25,7 @@ class Token extends Request implements TokenInterface, RetrieveInterface
         return $this;
     }
 
-    public function createBankAccount($country, $currency, $accountHolderName, $accountHolderType, $routingNumber, $accountNumber, $arguments = [], $key = null)
+    public function createBankAccount($country, $currency, string $accountHolderName, $accountHolderType, $routingNumber, $accountNumber, $arguments = [], $key = null)
     {
         $arguments['bank_account']['country'] = $country;
         $arguments['bank_account']['currency'] = $currency;
