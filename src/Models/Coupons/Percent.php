92d641cddee0458a11a9ece416a14a73f14e1dc2
Levi Durfee <levi.durfee@gmail.com>
2018-11-15 14:21:05 -0500

    Add Coupon types

---
+++ src/Models/Coupons/Percent.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Bulldog\Strype\Models\Coupons;
+
+use Bulldog\Strype\Contracts\Models\CouponTypeInterface;
+
+class Percent implements CouponTypeInterface
+{
+    protected $percentage;
+    protected $currency;
+
+    public function __construct(int $percentage, string $currency = 'usd')
+    {
+        if($percentage > 100 || 0 > $percentage) {
+            throw new \InvalidArgumentException('A positive float larger than 0, and smaller or equal to 100');
+        }
+
+        $this->percentage = $percentage;
+        $this->currency = $currency;
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'percent_off' => $this->percentage,
+            'currency' => $this->currency,
+        ];
+    }
+}
