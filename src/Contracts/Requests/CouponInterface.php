39d3237d12f45aaf4cd81912dd067b6c210ea615
Levi Durfee <levi.durfee@gmail.com>
2018-11-15 15:57:23 -0500

    Fix import

--- src/Contracts/Requests/CouponInterface.php
+++ src/Contracts/Requests/CouponInterface.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Bulldog\Strype\Contracts\Requests;
 
 use Bulldog\Strype\Contracts\Models\CouponTypeInterface;
-use Bulldog\Strype\Contracts\Models\DurationsInterface;
+use Bulldog\Strype\Contracts\Models\DurationInterface;
 
 interface CouponInterface extends \Bulldog\Strype\Contracts\RequestInterface
 {
