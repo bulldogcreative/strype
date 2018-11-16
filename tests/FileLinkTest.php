b86ac9983ffb4997416d585236a38ca677d590ad
Levi Durfee <levi.durfee@gmail.com>
2018-11-16 12:54:37 -0500

    Add FileLinkTest

---
+++ tests/FileLinkTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Strype;
+
+use Bulldog\Strype\Models\Files\BusinessLogo;
+
+class FileLinkTest extends TestCase
+{
+    public function testCreateLink()
+    {
+        $fp = fopen('./tests/files/Blank.jpg', 'r');
+        $logo = new BusinessLogo($fp);
+        $file = $this->strype->file()->create($logo, [], $this->id->get(12));
+        $fileLink = $this->strype->fileLink()->create($file->getId(), [], $this->id->get(12));
+
+        $this->assertEquals($file->getId(), $fileLink->file);
+        $this->assertEquals('file_link', $fileLink->object);
+    }
+
+    public function testRetrieveLink()
+    {
+        $fp = fopen('./tests/files/Blank.jpg', 'r');
+        $logo = new BusinessLogo($fp);
+        $file = $this->strype->file()->create($logo, [], $this->id->get(12));
+        $fileLink = $this->strype->fileLink()->create($file->getId(), [], $this->id->get(12));
+
+        $link = $this->strype->fileLink()->retrieve($fileLink->getId());
+        $this->assertEquals($fileLink->url, $link->url);
+    }
+
+    public function testUpdateLink()
+    {
+        $fp = fopen('./tests/files/Blank.jpg', 'r');
+        $logo = new BusinessLogo($fp);
+        $file = $this->strype->file()->create($logo, [], $this->id->get(12));
+        $fileLink = $this->strype->fileLink()->create($file->getId(), [], $this->id->get(12));
+        $link = $this->strype->fileLink()->retrieve($fileLink->getId());
+
+        $link->getResponse()->metadata['order_id'] = '6735';
+        $link->getResponse()->save();
+
+        $updated = $this->strype->fileLink()->retrieve($fileLink->getId());
+        $this->assertEquals('6735', $updated->metadata['order_id']);
+    }
+
+    public function testListAll()
+    {
+        $all = $this->strype->fileLink()->listAll(['limit' => 1]);
+        $this->assertEquals(1, count($all->data));
+    }
+}
