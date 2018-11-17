78693f6a48c189a3e2442bd07e52a3c4d01dd591
Levi Durfee <levi.durfee@gmail.com>
2018-11-16 21:35:34 -0500

    Fix create files test (#24)
    * Ignore clover file
    
    * Start working on file creation test
    
    * Remove dotenv
    
    * Update create file test

--- tests/FileCreationTest.php
+++ tests/FileCreationTest.php
@@ -6,26 +6,9 @@ use Bulldog\Strype\Models\Files\BusinessLogo;
 
 class FileCreationTest extends TestCase
 {
-    /**
-     * @before
-     */
-    public function setUpUploadBase()
-    {
-        \Stripe\Stripe::$apiUploadBase = \Stripe\Stripe::$apiBase;
-        \Stripe\Stripe::$apiBase = null;
-    }
-
-    /**
-     * @after
-     */
-    public function tearDownUploadBase()
-    {
-        \Stripe\Stripe::$apiBase = \Stripe\Stripe::$apiUploadBase;
-        \Stripe\Stripe::$apiUploadBase = 'https://files.stripe.com';
-    }
-
     public function testIsCreatableWithFileHandle()
     {
+        \Stripe\Stripe::setApiKey(getenv('STRIPE_API_KEY'));
         $this->expectsRequest(
             'post',
             '/v1/files',
@@ -40,27 +23,4 @@ class FileCreationTest extends TestCase
         $resource = $this->strype->file()->create($logo)->getResponse();
         $this->assertInstanceOf("Stripe\\File", $resource);
     }
-
-    // public function testIsCreatableWithCurlFile()
-    // {
-    //     if (!class_exists('\CurlFile', false)) {
-    //         // Older PHP versions don't support this
-    //         return;
-    //     }
-
-    //     $this->expectsRequest(
-    //         'post',
-    //         '/v1/files',
-    //         null,
-    //         ['Content-Type: multipart/form-data'],
-    //         true,
-    //         \Stripe\Stripe::$apiUploadBase
-    //     );
-    //     $curlFile = new \CurlFile(dirname(__FILE__) . '/../data/test.png');
-    //     $resource = File::create([
-    //         "purpose" => "dispute_evidence",
-    //         "file" => $curlFile,
-    //     ]);
-    //     $this->assertInstanceOf("Stripe\\File", $resource);
-    // }
 }
