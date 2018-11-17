f3368f71aff086291bc0568e48636e69220f1810
Levi Durfee <levi.durfee@gmail.com>
2018-11-17 17:42:10 -0500

    Document File class

--- src/Resources/File.php
+++ src/Resources/File.php
@@ -10,10 +10,26 @@ use Bulldog\Strype\Contracts\Traits\ListAllInterface;
 use Bulldog\Strype\Contracts\Models\FileTypeInterface;
 use Bulldog\Strype\Contracts\Traits\RetrieveInterface;
 
+/**
+ * File class.
+ *
+ * @see https://stripe.com/docs/api/files
+ */
 class File extends Resource implements FileInterface, RetrieveInterface, ListAllInterface
 {
     use Retrieve, ListAll;
 
+    /**
+     * Create a file.
+     *
+     * @see https://stripe.com/docs/api/files/create
+     *
+     * @param FileTypeInterface $file
+     * @param array             $arguments
+     * @param [type]            $key
+     *
+     * @return FileInterface
+     */
     public function create(FileTypeInterface $file, array $arguments = [], $key = null): FileInterface
     {
         $arguments['purpose'] = $file->getPurpose();
