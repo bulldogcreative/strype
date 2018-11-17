623684228113e1d368647a0ea8df5dfdeb7fc5e3
Levi Durfee <levi.durfee@gmail.com>
2018-11-17 09:53:09 -0500

    Add Balance docs

--- src/Resources/Balance.php
+++ src/Resources/Balance.php
@@ -13,6 +13,16 @@ use Bulldog\Strype\Contracts\Resources\BalanceInterface;
  */
 class Balance extends Resource implements BalanceInterface
 {
+    /**
+     * Retrieve balance.
+     *
+     * Retrieves the current account balance, based on the authentication that
+     * was used to make the request.
+     *
+     * @see https://stripe.com/docs/api/balance/balance_retrieve
+     *
+     * @return BalanceInterface
+     */
     public function retrieve(): BalanceInterface
     {
         $this->response = \Stripe\Balance::retrieve();
@@ -21,6 +31,18 @@ class Balance extends Resource implements BalanceInterface
         return $this;
     }
 
+    /**
+     * Retrieve a balance transaction.
+     *
+     * Retrieves the balance transaction with the given ID.
+     *
+     * @see https://stripe.com/docs/api/balance/balance_transaction_retrieve
+     *
+     * @param string $id The ID of the desired balance transaction, as found on
+     * any API object that affects the balance (e.g., a charge or transfer).
+     *
+     * @return BalanceInterface
+     */
     public function retrieveBalanceTransaction(string $id): BalanceInterface
     {
         $this->response = \Stripe\BalanceTransaction::retrieve($id);
