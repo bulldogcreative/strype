be9601f57d039b4ba7d0158fd32d522f3483639f
Levi Durfee <levi.durfee@gmail.com>
2018-11-17 17:33:32 -0500

    Document Discount class

--- src/Resources/Discount.php
+++ src/Resources/Discount.php
@@ -7,8 +7,25 @@ use Bulldog\Strype\Contracts\Resources\CustomerInterface;
 use Bulldog\Strype\Contracts\Resources\DiscountInterface;
 use Bulldog\Strype\Contracts\Resources\SubscriptionInterface;
 
+/**
+ * Discount class.
+ *
+ * @see https://stripe.com/docs/api/discounts
+ */
 class Discount extends Resource implements DiscountInterface
 {
+    /**
+     * Delete a customer discount.
+     *
+     * Removes the currently applied discount on a customer.
+     *
+     * @see https://stripe.com/docs/api/discounts/delete
+     *
+     * @param CustomerInterface $customer
+     * @param string            $key
+     *
+     * @return DiscountInterface
+     */
     public function deleteCustomerDiscount(CustomerInterface $customer, string $key = null): DiscountInterface
     {
         $this->customer($customer->getId(), $key);
@@ -17,6 +34,17 @@ class Discount extends Resource implements DiscountInterface
         return $this;
     }
 
+    /**
+     * Delete a subscription discount.
+     *
+     * Removes the currently applied discount on a subscription.
+     *
+     * @see https://stripe.com/docs/api/discounts/subscription_delete
+     *
+     * @param SubscriptionInterface $subscription
+     *
+     * @return DiscountInterface
+     */
     public function deleteSubscriptionDiscount(SubscriptionInterface $subscription): DiscountInterface
     {
         $this->subscription($subscription->getId());
