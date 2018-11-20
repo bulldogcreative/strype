b0fcf121200a613f9b518f03ef9aa3834b119405
Levi Durfee <levi.durfee@gmail.com>
2018-11-20 08:35:09 -0500

    Update Balance docs

--- src/Resources/Balance.php
+++ src/Resources/Balance.php
@@ -7,18 +7,20 @@ use Bulldog\Strype\Traits\Retrieve;
 use Bulldog\Strype\Contracts\Resources\BalanceInterface;
 
 /**
- * Class Balance.
- *
  * This is an object representing your Stripe balance. You can retrieve it to see
  * the balance currently on your Stripe account.
+ *
+ * You can also retrieve the balance history, which contains a list of transactions
+ * that contributed to the balance (charges, payouts, and so forth).
+ *
+ * The available and pending amounts for each currency are broken down further by
+ * payment source types.
  *
  * @see https://stripe.com/docs/api/balance
  */
 class Balance extends Resource implements BalanceInterface
 {
     /**
-     * Retrieve balance.
-     *
      * Retrieves the current account balance, based on the authentication that
      * was used to make the request.
      *
@@ -35,13 +37,11 @@ class Balance extends Resource implements BalanceInterface
     }
 
     /**
-     * Retrieve a balance transaction.
-     *
      * Retrieves the balance transaction with the given ID.
      *
      * @see https://stripe.com/docs/api/balance/balance_transaction_retrieve
      *
-     * @param string $id
+     * @param string $id The ID of the desired balance transaction
      *
      * @return BalanceInterface
      */
