4c212c90ad7449b2de5dba374d90372cbdd1b342
Levi Durfee <levi.durfee@gmail.com>
2018-11-20 13:03:30 -0500

    Update Charge interface

--- src/Contracts/Resources/ChargeInterface.php
+++ src/Contracts/Resources/ChargeInterface.php
@@ -2,10 +2,15 @@
 
 namespace Bulldog\Strype\Contracts\Resources;
 
+use Bulldog\Strype\Contracts\ResourceInterface;
+use Bulldog\Strype\Contracts\Traits\UpdateInterface;
+use Bulldog\Strype\Contracts\Traits\ListAllInterface;
+use Bulldog\Strype\Contracts\Traits\RetrieveInterface;
+
 /**
  * Interface CustomerInterface.
  */
-interface ChargeInterface extends \Bulldog\Strype\Contracts\ResourceInterface
+interface ChargeInterface extends ResourceInterface, RetrieveInterface, UpdateInterface, ListAllInterface
 {
     public function create(CustomerInterface $customer, int $amount, array $arguments = [], string $key = null, string $currency = 'usd'): ChargeInterface;
 
