4dcdc2adb3b0ba445b0f9347ea39a8b8eac226e0
Levi Durfee <levi.durfee@gmail.com>
2018-11-20 10:53:18 -0500

    Add File method docs

--- src/Models/File.php
+++ src/Models/File.php
@@ -12,11 +12,19 @@ abstract class File implements FileTypeInterface
     protected $file;
     protected $purpose;
 
+    /**
+     * Returns the stream resource.
+     */
     public function getFile()
     {
         return $this->file;
     }
 
+    /**
+     * Returns the purpose of the file.
+     *
+     * @return string
+     */
     public function getPurpose(): string
     {
         return $this->purpose;
