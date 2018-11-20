ae54b4c490014df4fd603609ca799b925540073f
Levi Durfee <levi.durfee@gmail.com>
2018-11-20 08:59:11 -0500

    Try inheritdoc

--- src/Resources/Balance.php
+++ src/Resources/Balance.php
@@ -13,6 +13,8 @@ use Bulldog\Strype\Contracts\Resources\BalanceInterface;
  * You can also retrieve the balance history, which contains a list of transactions
  * that contributed to the balance (charges, payouts, and so forth).
  *
+ * @var id {@inheritDoc}
+ *
  * @see https://stripe.com/docs/api/balance
  */
 class Balance extends Resource implements BalanceInterface
