fae977ec83aea980b7b7c96191ab631cb89ad542
Levi Durfee <levi.durfee@gmail.com>
2018-11-20 08:45:16 -0500

    Remove extra line

--- src/Resources/Balance.php
+++ src/Resources/Balance.php
@@ -13,9 +13,6 @@ use Bulldog\Strype\Contracts\Resources\BalanceInterface;
  * You can also retrieve the balance history, which contains a list of transactions
  * that contributed to the balance (charges, payouts, and so forth).
  *
- * The available and pending amounts for each currency are broken down further by
- * payment source types.
- *
  * @see https://stripe.com/docs/api/balance
  */
 class Balance extends Resource implements BalanceInterface
