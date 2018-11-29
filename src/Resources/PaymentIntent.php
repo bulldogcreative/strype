5c43480893e55669a1b55a4fe5abae3f27e8a867
Levi Durfee <levi.durfee@gmail.com>
2018-11-29 16:36:27 -0500

    Add PaymentIntent Resource

---
+++ src/Resources/PaymentIntent.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Bulldog\Strype\Resources;
+
+use Bulldog\Strype\Resource;
+use Bulldog\Strype\Traits\Retrieve;
+use Bulldog\Strype\Contracts\Resources\PaymentIntentInterface;
+
+/**
+ * @see https://stripe.com/docs/api/payment_intents
+ */
+class Balance extends Resource implements PaymentIntentInterface
+{
+
+}
