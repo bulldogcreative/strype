53f494600e9d2cfaf86c76d5da6b25a47db68369
Levi Durfee <levi.durfee@gmail.com>
2018-11-30 15:15:33 -0500

    Add Create PaymentIntent test

--- tests/PaymentIntentTest.php
+++ tests/PaymentIntentTest.php
@@ -4,5 +4,14 @@ namespace Strype;
 
 class PaymentIntentTest extends TestCase
 {
+    public function testCreatePaymentIntent()
+    {
+        $pi = $this->strype->paymentIntent()->create(
+            ['card'],
+            9999
+        );
 
+        $this->assertEquals('payment_intent', $pi->object);
+        $this->assertEquals('succeeded', $pi->status);
+    }
 }
