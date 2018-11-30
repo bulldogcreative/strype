25cda0db8feca2176f1fe40a3416731bc6674e40
Levi Durfee <levi.durfee@gmail.com>
2018-11-30 14:33:08 -0500

    Add PaymentIntent create method interface

--- src/Contracts/Resources/PaymentIntentInterface.php
+++ src/Contracts/Resources/PaymentIntentInterface.php
@@ -6,4 +6,5 @@ use Bulldog\Strype\Contracts\ResourceInterface;
 
 interface PaymentIntentInterface extends ResourceInterface
 {
+    public function create(array $allowedSourceTypes, int $amount, array $arguments = [], string $key = null, string $currency = 'usd'): PaymentIntentInterface;
 }
