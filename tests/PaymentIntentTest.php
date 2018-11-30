6142e4e7f0f38d84a7c6dbe92d68034034fe3fe6
Levi Durfee <levi.durfee@gmail.com>
2018-11-30 15:21:28 -0500

    Test PaymentIntent Retrieve

--- tests/PaymentIntentTest.php
+++ tests/PaymentIntentTest.php
@@ -14,4 +14,16 @@ class PaymentIntentTest extends TestCase
         $this->assertEquals('payment_intent', $pi->object);
         $this->assertEquals('succeeded', $pi->status);
     }
+
+    public function testRetrievePaymentIntent()
+    {
+        $pi = $this->strype->paymentIntent()->create(
+            ['card'],
+            9999
+        );
+
+        $response = $pi->retrieve($pi->id);
+        $this->assertEquals('payment_intent', $response->object);
+        $this->assertEquals('succeeded', $response->status);
+    }
 }
