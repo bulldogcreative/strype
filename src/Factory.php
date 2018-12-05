2d1380510824745df26d98a2f4044fc89a372228
Levi Durfee <levi.durfee@gmail.com>
2018-12-05 16:32:11 -0500

    Add method stubs

--- src/Factory.php
+++ src/Factory.php
@@ -1,3 +1,91 @@
 <?php
 
+namespace Bulldog\Strype;
 
+class Factory
+{
+    public function couponAmount()
+    {
+
+    }
+
+    public function couponPercent()
+    {
+
+    }
+
+    public function durationForever()
+    {
+
+    }
+
+    public function durationOnce()
+    {
+
+    }
+
+    public function durationRepeating()
+    {
+
+    }
+
+    public function fileBusinessLogo()
+    {
+
+    }
+
+    public function fileCustomerSignature()
+    {
+
+    }
+
+    public function fileDisputeEvidence()
+    {
+
+    }
+
+    public function fileIdentityDocument()
+    {
+
+    }
+
+    public function filePciDocument()
+    {
+
+    }
+
+    public function fileTaxDocumentUserUpload()
+    {
+
+    }
+
+    public function invoiceItemAmount()
+    {
+
+    }
+
+    public function invoiceItemQuantity()
+    {
+
+    }
+
+    public function productGood()
+    {
+
+    }
+
+    public function productService()
+    {
+
+    }
+
+    public function subscriptionChargeAutomatically()
+    {
+
+    }
+
+    public function subscriptionInvoice()
+    {
+
+    }
+}
